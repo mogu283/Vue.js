3e3daa570b76aa735689336637cd77fbf2de70c5
mogu283 <mogukona2016@gmail.com>
2020-06-23 17:31:59 +0900

    Initial Commit

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+
+
+<template>
+    <div class="header">
+<button @click="showText">表示</button>
+<p>{{text}}</p>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            text:""
+        }
+    },
+    props:["parentText"],
+    methods:{
+        showText(){
+this.text=this.parentText
+        }
+    }
+
+}
+</script>
